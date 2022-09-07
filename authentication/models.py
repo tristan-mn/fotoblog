b33dad5be7b1ad10b00a80e1f20e20ad4613c2f4
Tristan Montemitro <tmontemitro@gmail.com>
2022-09-07 07:54:17 +0200

    personnalidation du model utilisateur et création de la page de login

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,19 @@
+from typing import Any
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'créateur'),
+        (SUBSCRIBER, 'abonné'),
+    )
+
+    profile_photo = models.ImageField(verbose_name='photo de profile')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from authentication import forms
 
 # Create your views here.
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                message = "identifiant ou mot de passe invalide"
+    return render(
+        request, 'authentication/login.html', context={'form': form, 'message': message}
+    )
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
